b641aa1f18a25a9f44ecb936d47c446efb438223
Williamrobert Green <wkgreen13@gmail.com>
2020-01-15 13:57:28 -0500

    adds inti item endpoint, get by id

---
+++ api/handlers.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/kjintroverted/wizardsBrew/api/items"
+	"github.com/kjintroverted/wizardsBrew/psql"
+
+	"github.com/gorilla/mux"
+)
+
+// Items handles all Item req
+func Items(w http.ResponseWriter, r *http.Request) {
+	db, err := psql.NewPostgresConnection()
+	if err != nil {
+		fmt.Println("ERR", err)
+	}
+	defer db.Close()
+	service := items.NewItemService(items.NewItemRepo(db))
+
+	// LOAD PATH PARAMS
+	pathParams := mux.Vars(r)
+
+	if id, ok := pathParams["id"]; ok { // GET ONE
+		item, err := service.FindByID(id)
+		if err != nil {
+			goto Fail
+		}
+		res, _ := json.Marshal(item)
+		w.Write(res)
+	}
+
+Fail:
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte(err.Error()))
+}
