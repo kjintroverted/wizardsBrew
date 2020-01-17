5e680c6f514780ff1b6478ead1a988e96bea3116
Williamrobert Green <wkgreen13@gmail.com>
2020-01-17 12:31:16 -0500

    adds character model

---
+++ api/character/model.go
@@ -0,0 +1,39 @@
+package character
+
+// PC describes the data for a Playable Character
+type PC struct {
+	ID           int        `json:"ID" db:"ID"`
+	Owner        string     `json:"owner" db:"owner"`
+	AuthUsers    []string   `json:"authUsers" db:"auth_users"`
+	Name         string     `json:"name" db:"name"`
+	RaceID       int        `json:"raceID" db:"race_id"`
+	ClassID      int        `json:"classID" db:"class_id"`
+	Stats        stats      `json:"stats" db:"stats"`
+	XP           int        `json:"xp" db:"xp"`
+	HP           int        `json:"hp" db:"hp"`
+	MaxHP        int        `json:"maxHP" db:"maxHP"`
+	Init         int        `json:"initiative" db:"initiative"`
+	ProSkills    []proSkill `json:"proSkills" db:"proSkills"`
+	ProTools     []string   `json:"proTools" db:"proTools"`
+	Lang         []string   `json:"languages" db:"languages"`
+	EquipmentIDs []int      `json:"equipmentIDs" db:"equipmentIDs"`
+	WeaponIDs    []int      `json:"weaponIDs" db:"weaponIDs"`
+	InventoryIDs []int      `json:"inventoryIDs" db:"inventoryIDs"`
+	Gold         float64    `json:"gold" db:"gold"`
+	SpellIDs     []int      `json:"spellIDs" db:"spellIDs"`
+	SpecFeatIDs  []int      `json:"specFeatIDs" db:"specFeatIDs"`
+}
+
+type stats struct {
+	STR int `json:"str" db:"str"`
+	DEX int `json:"dex" db:"dex"`
+	CON int `json:"con" db:"con"`
+	INT int `json:"int" db:"int"`
+	WIS int `json:"wis" db:"wis"`
+	CHA int `json:"cha" db:"cha"`
+}
+
+type proSkill struct {
+	Name string `json:"name" db:"name"`
+	Mult int    `json:"multiplier" db:"multiplier"`
+}
