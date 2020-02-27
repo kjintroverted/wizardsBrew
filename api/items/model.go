90f311d8708065c30cdd8c9678a431851df289e4
Williamrobert Green <wkgreen13@gmail.com>
2020-02-27 15:07:07 -0500

    adds homebrew to model

--- api/items/model.go
+++ api/items/model.go
@@ -6,16 +6,17 @@ import (
 
 // Item describes gear, weapons, and armor
 type Item struct {
-	ID     string             `json:"id" db:"id"`
-	Name   string             `json:"name" db:"name"`
-	Type   string             `json:"type" db:"type"`
-	Cost   *psql.NullFloat    `json:"cost" db:"cost"`
-	Weight *psql.NullFloat    `json:"weight" db:"weight"`
-	Attune *psql.NullString   `json:"attune" db:"attune"`
-	Rarity *psql.NullString   `json:"rarity" db:"rarity"`
-	Weapon *weaponInfo        `json:"weapon,omitempty" db:"weapon"`
-	AC     *psql.NullInt      `json:"ac,omitempty" db:"ac"`
-	Info   []psql.Description `json:"info,omitempty" db:"info"`
+	ID         string             `json:"id" db:"id"`
+	Name       string             `json:"name" db:"name"`
+	Type       string             `json:"type" db:"type"`
+	Cost       *psql.NullFloat    `json:"cost" db:"cost"`
+	Weight     *psql.NullFloat    `json:"weight" db:"weight"`
+	Attune     *psql.NullString   `json:"attune" db:"attune"`
+	Rarity     *psql.NullString   `json:"rarity" db:"rarity"`
+	Weapon     *weaponInfo        `json:"weapon,omitempty" db:"weapon"`
+	AC         *psql.NullInt      `json:"ac,omitempty" db:"ac"`
+	Info       []psql.Description `json:"info,omitempty" db:"info"`
+	IsHomebrew bool               `json:"isHomebrew" db:"homebrew"`
 }
 
 type weaponInfo struct {
